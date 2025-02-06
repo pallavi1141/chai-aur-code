19e8297ba45b04a1ac472b89f904747368857f50
Your Name <your-email@example.com>
2025-02-06 18:15:24 +0530

    add some features

--- video 17_objects_part2/script.js
+++ video 17_objects_part2/script.js
@@ -7,11 +7,11 @@ tinderUser.id = "123abc";
 tinderUser.name = "sammy";
 tinderUser.isLoggedIn = "false";
 
-console.log(tinderUser);
-console.log(object.keys(tinderUser));
-console.log(object.value(tinderUser));
-console.log(object.entries(tinderUser));
-console.log(tinderUser.hasOwnproperty('isLoggedIn'));
+// console.log(tinderUser);
+// console.log(object.keys(tinderUser));
+// console.log(object.value(tinderUser));
+// console.log(object.entries(tinderUser));
+// console.log(tinderUser.hasOwnproperty("isLoggedIn"));
 
 const regularUser = {
   email: "some@gmail.com",
@@ -23,14 +23,10 @@ const regularUser = {
   },
 };
 
-
-console.log(regularUser.email);
-console.log(regularUser.fullname);
-console.log(regularUser.fullname.username.firstname);
-console.log(regularUser.fullname.username.lastname);
-
-
-
+// console.log(regularUser.email);
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.username.firstname);
+// console.log(regularUser.fullname.username.lastname);
 
 //merge/combine the value
 const obj1 = { 1: "a", 2: "b" };
@@ -41,8 +37,6 @@ const obj3 = { 5: "e", 6: "f" };
 // const obj4 = object.assign({}, obj1, obj2, obj3);
 const obj4 = { ...obj1, ...obj2, ...obj3 };
 // console.log(obj4);
-
-
 
 const users = [
   {
@@ -63,3 +57,35 @@ users[3].id;
 
 
 
+
+//video 18.... de-structure
+
+const course = {
+  courseName: "js in hindi",
+  price: "999",
+  courseInstructor: "sheri"
+  
+};
+
+//accessed
+course.courseInstructor
+
+const {courseInstructor:instructor} = course
+console.log(instructor);
+
+
+//jSON API(keys and value both in the string) As a object format
+{
+  "name": "Ruhi",
+  "courseName":"js in hindi",
+  "price":"free"
+
+}
+
+//array format
+
+[
+  {},
+  {},
+  {},
+]
